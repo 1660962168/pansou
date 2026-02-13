20e2e6471fb9b53094db1ae4165d3d00e01c49cd
Qiudaoyu <1660962168@qq.com>
2026-02-13 17:56:41 +0800

    更新

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -4,5 +4,9 @@ bp = Blueprint('admin', __name__, url_prefix='/admin')
 
 # 访问路径: /admin/
 @bp.route('/')
-def index():
-    return "这是管理员后台首页"
+def admin_index():
+    return render_template('admin/index.html')
+
+@bp.route('/login')
+def login():
+    return render_template('admin/logn.html')
